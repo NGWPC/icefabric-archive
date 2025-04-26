1fa9f5aab88c07172d4ece299a2e1e32aa9e9ab3
taddyb <tkb5476@psu.edu>
2025-04-25 21:26:26 -0400

    wrote an example for building my warehouse DB

---
+++ scripts/gpkg_to_parquet.py
@@ -0,0 +1,14 @@
+"""A simple script to convert the v2.2 hydrofabric to parquet"""
+
+import geopandas as gpd
+import fiona
+
+input_file = "data/conus_nextgen.gpkg"
+available_layers = fiona.listlayers(input_file)
+print(f"Layers in GeoPackage: {available_layers}")
+
+for layer in available_layers:
+    gdf = gpd.read_file(input_file, layer=layer)
+    output_file = f"data/parquet/{layer}.parquet"
+    gdf.to_parquet(output_file)    
+    print(f"Converted layer '{layer}' to {output_file}")
