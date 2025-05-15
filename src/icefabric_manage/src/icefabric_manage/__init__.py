04e452bcd4c39613c32b5a69cbaadcc5b3139863
SylC-ray <201017958+SylC-ray@users.noreply.github.com>
2025-05-15 15:51:45 -0400

    Update __init__.py
    Adding imports of Classes in __init__.py

--- src/icefabric_manage/src/icefabric_manage/__init__.py
+++ src/icefabric_manage/src/icefabric_manage/__init__.py
@@ -1,3 +1,5 @@
 from icefabric_manage._api import build
+from icefabric_manage.iceberg_table import IcebergTable
+from icefabric_manage.map_fim_data import MapData
 
-__all__ = ["build"]
+__all__ = ["build", "IcebergTable", "MapData"]
