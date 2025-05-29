c7f54124309b5c07b03bf95ce1cf0cdd08735edd
taddyb <tkb5476@psu.edu>
2025-05-28 22:21:46 -0400

    updated testing docs

--- src/icefabric_tools/tests/test_utils.py
+++ src/icefabric_tools/tests/test_utils.py
@@ -6,7 +6,15 @@ from icefabric_tools import IdType, find_origin
 
 @pytest.mark.unit
 def test_find_origin(hydrofabric_catalog: Catalog):
-    """Testing to see if there is an error thrown with a non-existent ID"""
+    """Testing the find origin function"""
+    network = hydrofabric_catalog.load_table("hydrofabric.network")
+    with pytest.raises(ValueError, match="No origin found"):
+        find_origin(
+            network_table=network,
+            identifier="non-existent-id-12345",
+            id_type=IdType.HL_URI.value,
+        )
+
     network = hydrofabric_catalog.load_table("hydrofabric.network")
     with pytest.raises(ValueError, match="No origin found"):
         find_origin(
