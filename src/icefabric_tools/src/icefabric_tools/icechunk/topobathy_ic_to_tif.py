d51e1c539de738036a5df4de277393dc04fee12c
Quercus <quercus.hamlin@outlook.com>
2025-06-26 14:58:41 -0400

    config update

--- src/icefabric_tools/src/icefabric_tools/icechunk/topobathy_ic_to_tif.py
+++ src/icefabric_tools/src/icefabric_tools/icechunk/topobathy_ic_to_tif.py
@@ -7,12 +7,17 @@ load_dotenv()
 
 
 def convert_topobathy_to_tiff(output_dir: str, ic_rasters: list[NGWPCLocations]) -> None:
+    """Converts topobathy layers from icechunk to tiff for use in tiles
+
+    Parameters
+    ----------
+    output_dir : str
+        Directory to save outputs to
+    ic_rasters : list[NGWPCLocations]
+        list of NGWPCLocation raster paths. eg. [NGWPCLocations[TOPO_AK_30M_IC].path]
+    """
     for ic_raster in ic_rasters:
-        try:
-            repo = IcechunkS3Repo(location=NGWPCLocations[ic_raster].path)
-        except Exception as e:
-            raise e
-        print(repo)
+        repo = IcechunkS3Repo(location=NGWPCLocations[ic_raster].path)
         output = os.path.join(output_dir, f"{str.split(str(NGWPCLocations[ic_raster].path), '/')[-1]}.tif")
 
         repo.retrieve_and_convert_to_tif(dest=output, var_name='elevation')
@@ -21,6 +26,8 @@ def convert_topobathy_to_tiff(output_dir: str, ic_rasters: list[NGWPCLocations])
 if __name__ == '__main__':
     output_dir = "./temp_tb_data"
     os.makedirs(output_dir, exist_ok=True)
+
+    # all 30 m topobathy layers
     ic_rasters = [f for f in NGWPCLocations._member_names_ if "TOPO" and "30M" in f][2:3]
 
     convert_topobathy_to_tiff(output_dir, ic_rasters)
