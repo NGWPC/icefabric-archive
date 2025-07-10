4b3f28487261933f001140fc6a331c22c5336990
SylC-ray <201017958+SylC-ray@users.noreply.github.com>
2025-07-10 16:59:11 -0400

    ras xs router added to app

--- app/main.py
+++ app/main.py
@@ -13,6 +13,7 @@ from app.routers.module_params.router import api_router as module_params_router
 from app.routers.nwm_modules.router import sft_router, topoflow_router
 from app.routers.ras_xs.router import api_router as ras_api_router
 from app.routers.streamflow_observations.router import api_router as streamflow_api_router
+from app.routers.ras_xs.router import api_router as ras_api_router
 from icefabric.helpers import load_creds
 
 
@@ -53,7 +54,6 @@ app.include_router(sft_router, prefix="/v2")
 app.include_router(topoflow_router, prefix="/v2")
 app.include_router(module_params_router, prefix="/v1")
 app.include_router(ras_api_router, prefix="/v1")
-
 
 @app.head(
     "/health",
