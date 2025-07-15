d3c63fdd03eb436f1d2b5f24438e13f74cea1e2d
SylC-ray <201017958+SylC-ray@users.noreply.github.com>
2025-07-15 11:04:35 -0400

    Update __init__.py

--- src/icefabric/__init__.py
+++ src/icefabric/__init__.py
@@ -1,6 +1,6 @@
 """An Apache Iceberg + Icechunk implementation of Hydrofabric data services"""
 
-from . import builds, helpers, hydrofabric, modules, ras_xs, schemas, ui
+from . import builds, cli, helpers, hydrofabric, modules, schemas, ui, ras_xs
 from ._version import __version__
 
-__all__ = ["__version__", "builds", "hydrofabric", "helpers", "modules", "schemas", "ui", "ras_xs"]
+__all__ = ["__version__", "builds", "cli", "hydrofabric", "helpers", "modules", "schemas", "ui",  "ras_xs"]
