1b9930525e8002988969a3878d31bdf4b755bee0
taddyb <tkb5476@psu.edu>
2025-07-21 14:16:55 -0400

    feat: cli tool for building an upstread json file

--- src/icefabric/builds/__init__.py
+++ src/icefabric/builds/__init__.py
@@ -2,9 +2,11 @@
 
 from .build import build_iceberg_table
 from .icechunk_s3_module import IcechunkRepo, S3Path
+from .upstream_lookup_table import build_upstream_json
 
 __all__ = [
     "build_iceberg_table",
+    "build_upstream_json",
     "IcechunkRepo",
     "S3Path",
 ]
